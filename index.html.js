330ab5f0b455dc68c820991589e3b7b455a06542
= <craig@craigshoemaker.net>
2018-12-18 15:09:36 -0500

    Add files

---
+++ index.html.js
@@ -0,0 +1,30 @@
+const SERVER_LOCATION = 'https://spinefeed.azurewebsites.net'; //'http://localhost:7071'
+const SPINEFEED_URL = `${SERVER_LOCATION}/api/article`;
+
+const app = new Vue({
+    el: '#app',
+    data: { 
+        article: '',
+        markup: ''
+    },
+    methods: {
+        getFeedback: async (contentType, output) => {
+            try {
+                contentType = 'quickstart';
+                output = 'html';
+
+                const url = `${SPINEFEED_URL}?type=${contentType}&output=${output}`;
+                const headers = { 'Content-Type': 'application/json' };
+                const data = app.article;
+    
+                const response = await axios.post(url, data, headers);
+
+                console.log(response.data.details);
+                
+                app.markup = response.data.details;
+            } catch(ex) {
+                console.log(ex);
+            }
+        }
+    }
+})
